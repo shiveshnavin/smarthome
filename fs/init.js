b8642e60f30a756c14e35f64a70030ec8405136c
shiveshnavin <shiveshnavin@gmail.com>
2020-09-14 22:50:55 +0530

    fused demojs

--- fs/init.js
+++ fs/init.js
@@ -1,112 +1,63 @@
-load('api_aws.js');
-load('api_azure.js');
-load('api_config.js');
-load('api_dash.js');
-load('api_events.js');
-load('api_gcp.js');
-load('api_gpio.js');
-load('api_mqtt.js');
-load('api_shadow.js');
+print('>>> Hello from firmware ');
+load('api_config.js'); 
+load('api_events.js'); 
+load('api_gpio.js'); 
 load('api_timer.js');
-load('api_sys.js');
-load('api_watson.js');
+load('api_sys.js'); 
+load('api_rpc.js'); 
+load('api_esp32_touchpad.js');
+load('api_adc.js'); 
 
-let btn = Cfg.get('board.btn1.pin');              // Built-in button GPIO
-let led = Cfg.get('board.led1.pin');              // Built-in LED GPIO number
-let onhi = Cfg.get('board.led1.active_high');     // LED on when high?
-let state = {on: false, btnCount: 0, uptime: 0};  // Device state
-let online = false;                               // Connected to the cloud?
 
-let setLED = function(on) {
-  let level = onhi ? on : !on;
+let sensor=4;
+let led = 2;
+let purple = 5;
+let orange=18;
+let blue=19;
+let yellow=21;
+
+let motor = 19;
+let gate = 21;
+
+ADC.enable(sensor);
+GPIO.set_mode(led, GPIO.MODE_OUTPUT);
+GPIO.set_mode(purple, GPIO.MODE_OUTPUT);
+GPIO.set_mode(orange, GPIO.MODE_OUTPUT);
+GPIO.set_mode(blue, GPIO.MODE_OUTPUT);
+GPIO.set_mode(yellow, GPIO.MODE_OUTPUT);
+
+let setPin = function(on,level) { 
   GPIO.write(led, level);
   print('LED on ->', on);
+  return true;
 };
 
-GPIO.set_mode(led, GPIO.MODE_OUTPUT);
-setLED(state.on);
 
-let reportState = function() {
-  Shadow.update(0, state);
-};
+let ts = TouchPad.GPIO[sensor];
 
-// Update state every second, and report to cloud if online
-Timer.set(1000, Timer.REPEAT, function() {
-  state.uptime = Sys.uptime();
-  state.ram_free = Sys.free_ram();
-  print('online:', online, JSON.stringify(state));
-  if (online) reportState();
-}, null);
+TouchPad.init();
+TouchPad.setVoltage(TouchPad.HVOLT_2V4, TouchPad.LVOLT_0V8, TouchPad.HVOLT_ATTEN_1V5);
+TouchPad.config(ts, 0);
+// Timer.set(1000 /* 1 sec */, Timer.REPEAT, function() {
+ 
+// }, null);
 
-// Set up Shadow handler to synchronise device state with the shadow state
-Shadow.addHandler(function(event, obj) {
-  if (event === 'UPDATE_DELTA') {
-    print('GOT DELTA:', JSON.stringify(obj));
-    for (let key in obj) {  // Iterate over all keys in delta
-      if (key === 'on') {   // We know about the 'on' key. Handle it!
-        state.on = obj.on;  // Synchronise the state
-        setLED(state.on);   // according to the delta
-      } else if (key === 'reboot') {
-        state.reboot = obj.reboot;      // Reboot button clicked: that
-        Timer.set(750, 0, function() {  // incremented 'reboot' counter
-          Sys.reboot(500);                 // Sync and schedule a reboot
-        }, null);
-      }
-    }
-    reportState();  // Report our new state, hopefully clearing delta
-  }
+
+RPC.addHandler('read_touch', function(args) {
+  let tv = TouchPad.read(ts);
+  print('Sensor', ts, 'value', tv);
+  return tv;
 });
 
-if (btn >= 0) {
-  let btnCount = 0;
-  let btnPull, btnEdge;
-  if (Cfg.get('board.btn1.pull_up') ? GPIO.PULL_UP : GPIO.PULL_DOWN) {
-    btnPull = GPIO.PULL_UP;
-    btnEdge = GPIO.INT_EDGE_NEG;
-  } else {
-    btnPull = GPIO.PULL_DOWN;
-    btnEdge = GPIO.INT_EDGE_POS;
+RPC.addHandler('read_adc', function(args) {
+  if(args.pin!==sensor){
+    ADC.enable(args.pin);
   }
-  GPIO.set_button_handler(btn, btnPull, btnEdge, 20, function() {
-    state.btnCount++;
-    let message = JSON.stringify(state);
-    let sendMQTT = true;
-    if (Azure.isConnected()) {
-      print('== Sending Azure D2C message:', message);
-      Azure.sendD2CMsg('', message);
-      sendMQTT = false;
-    }
-    if (GCP.isConnected()) {
-      print('== Sending GCP event:', message);
-      GCP.sendEvent(message);
-      sendMQTT = false;
-    }
-    if (Watson.isConnected()) {
-      print('== Sending Watson event:', message);
-      Watson.sendEventJSON('ev', {d: state});
-      sendMQTT = false;
-    }
-    if (Dash.isConnected()) {
-      print('== Click!');
-      // TODO: Maybe do something else?
-      sendMQTT = false;
-    }
-    // AWS is handled as plain MQTT since it allows arbitrary topics.
-    if (AWS.isConnected() || (MQTT.isConnected() && sendMQTT)) {
-      let topic = 'devices/' + Cfg.get('device.id') + '/events';
-      print('== Publishing to ' + topic + ':', message);
-      MQTT.pub(topic, message, 0 /* QoS */);
-    } else if (sendMQTT) {
-      print('== Not connected!');
-    }
-  }, null);
-}
+  return ADC.read(args.pin);
+});
 
-Event.on(Event.CLOUD_CONNECTED, function() {
-  online = true;
-  Shadow.update(0, {ram_total: Sys.total_ram()});
-}, null);
+RPC.addHandler('write_pin', function(args) {
+  return setPin(args.pin,args.value);
+});
 
-Event.on(Event.CLOUD_DISCONNECTED, function() {
-  online = false;
-}, null);
+print('>>> Registered Handlers');
