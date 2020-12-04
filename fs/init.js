73b80ebd9414dca6b3df4bc640d1d93515f0a641
shiveshnavin <shiveshnavin@gmail.com>
2020-12-04 23:04:46 +0530

    Firmware v3

--- fs/init.js
+++ fs/init.js
@@ -1,20 +1,20 @@
 print('>>> Hello from firmware ');
-load('api_config.js'); 
-load('api_events.js'); 
-load('api_gpio.js'); 
+load('api_config.js');
+load('api_events.js');
+load('api_gpio.js');
 load('api_timer.js');
-load('api_sys.js'); 
-load('api_rpc.js'); 
+load('api_sys.js');
+load('api_rpc.js');
 // load('api_esp32_touchpad.js');
 // load('api_adc.js'); 
 
 
-let sensor=4;
+let sensor = 4;
 let led = 2;
 let purple = 5;
-let orange=18;
-let blue=19;
-let yellow=21;
+let orange = 18;
+let blue = 19;
+let yellow = 21;
 
 let motor = 19;
 let gate = 21;
@@ -26,14 +26,14 @@ GPIO.set_mode(orange, GPIO.MODE_OUTPUT);
 GPIO.set_mode(blue, GPIO.MODE_OUTPUT);
 GPIO.set_mode(yellow, GPIO.MODE_OUTPUT);
 
-GPIO.write(purple,1);
-GPIO.write(orange,1);
-GPIO.write(blue,1);
-GPIO.write(yellow,1); 
+GPIO.write(purple, 1);
+GPIO.write(orange, 1);
+GPIO.write(blue, 1);
+GPIO.write(yellow, 1);
 
-let setPin = function(on,level) { 
-  GPIO.write(led, level);
-  print('LED on ->', on);
+let setPin = function (pin, level) {
+  GPIO.write(pin, level);
+  print('Set pin = ', pin, ' to ', level);
   return true;
 };
 
@@ -44,7 +44,7 @@ let setPin = function(on,level) {
 // TouchPad.setVoltage(TouchPad.HVOLT_2V4, TouchPad.LVOLT_0V8, TouchPad.HVOLT_ATTEN_1V5);
 // TouchPad.config(ts, 0);
 // Timer.set(1000 /* 1 sec */, Timer.REPEAT, function() {
- 
+
 // }, null);
 
 
@@ -61,19 +61,84 @@ let setPin = function(on,level) {
 //   return ADC.read(args.pin);
 // });
 
-RPC.addHandler('read_pin', function(args) {
+
+let keepAliveTick = Cfg.get('smarthome.hearbeat');
+let isAutoMode = Cfg.get('smarthome.automode');
+let minautoretry = Cfg.get('smarthome.minautoretry');
+let lastTryTime = Cfg.get("smarthome.lastautotry");
+let runautofor = 1000 * Cfg.get("smarthome.runautofor");
+
+let autostarthour = Cfg.get('smarthome.autostarthour');
+let autoendhour = Cfg.get('smarthome.autoendhour');
+
+let autoPin = motor;
+if (lastTryTime < 1607099824) {
+
+  Cfg.set({ smarthome: { lastautotry: Timer.now() } });
+
+}
+
+Timer.set(keepAliveTick, Timer.REPEAT, function () {
+
+  if (isAutoMode) {
+    let now = Timer.now();
+    if (now - lastTryTime > minautoretry) {
+      let curHour = JSON.parse(Timer.fmt("%H:%m", Timer.now()).slice(0, 2));
+      let isItTimeYet = curHour >= autostarthour && curHour <= autostarthour;
+      if (isItTimeYet) {
+
+        lastTryTime = now;
+        Cfg.set({ smarthome: { lastautotry: now } });
+        print('Try Motor Now !!!');
+        setPin(autoPin, 0);
+        stopMotorByPullingUpPin(autoPin, 1, runautofor);
+
+      }
+    }
+    // print('Hearbeat ! last try = ', lastTryTime);
+
+  }
+
+}, null);
+
+let lastPin = 0;
+let lastValue = 0;
+let stopMotorByPullingUpPin = function (pin, value, timeOut) {
+  lastPin = pin;
+  lastValue = value;
+  let delay = Cfg.get('smarthome.stoptimeout');
+  if (timeOut !== undefined) {
+    delay = timeOut;
+  }
+  Timer.set(delay, 0, function () {
+    setPin(lastPin, lastValue);
+  }, null);
+};
+
+RPC.addHandler('read_pin', function (args) {
   return GPIO.read(args.pin);
 });
 
-RPC.addHandler('write_pin', function(args) {
-  return setPin(args.pin,args.value);
+RPC.addHandler('write_pin', function (args) {
+  return setPin(args.pin, args.value);
 });
 
+RPC.addHandler('write_pin_auto', function (args) {
+  stopMotorByPullingUpPin(args.pin, 1);
+  return setPin(args.pin, args.value);
+});
 
-RPC.addHandler('status', function(args) {
+RPC.addHandler('set_status', function (args) {
+  return Cfg.set(args.object);
+});
+
+RPC.addHandler('status', function (args) {
   let curtime = Timer.now();
-  let license = Cfg.get("device.license");
-  return {time:curtime,auto:license};
+  let config = Cfg.get("smarthome");
+  let now = Timer.now();
+  let fullTime = Timer.fmt("%Y-%m-%d %H:%M:%S", now);
+
+  return { time: curtime, config: config, time_fmt: fullTime };
 });
 
 print('>>> Registered Handlers');
--- set.sh
+++ set.sh
@@ -1,1 +1,6 @@
-sudo mos wifi Swati_Niwas_2.4G mother1919 && mos config-set dash.enable=true dash.token=TTyc8FAuEpARlATtrQh90Ig
+#!/bin/sh
+sudo mos build --platform esp32
+sudo mos flash --platform esp32
+sudo mos wifi wifi_my_ssid paswd 
+sudo mos config-set dash.enable=true dash.token=tOkeNNNN00193
+sudo mos console
