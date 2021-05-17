93c3fdcbfa2ea6453e92b29dee0ff45f21f955af
shiveshnavin <shiveshnavin@gmail.com>
2021-05-17 12:20:35 +0530

    Cleanup

--- set.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# sudo mos build --platform esp32
-# sudo mos flash --platform esp32
-sudo mos wifi Swati_Niwas_2.4G mother1919 
-sudo mos config-set dash.enable=true dash.token=LaFbVvJtxMubQVKi90sWXqQ
-sudo mos console
